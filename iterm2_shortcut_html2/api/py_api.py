36dc6ad87985b32547eb1e5157d1854d0bfb9e22
熊闯 <xiongchuang@vip.qq.com>
2025-05-13 08:56:34 +0800

    调整

--- iterm2_shortcut_html2/api/py_api.py
+++ iterm2_shortcut_html2/api/py_api.py
@@ -97,11 +97,14 @@ class PyApi:
         return await session.async_get_selection_text(selection)
 
     async def exec_shell(self, shell_text: str) -> Tuple[bool, str]:
-        p = subprocess.Popen(shell_text, shell=True, stdout=subprocess.PIPE)  # 执行shell语句并定义输出格式
+        env = os.environ.copy()
+        env["PATH"] = f"/usr/local/bin:/opt/homebrew/bin:{os.path.expanduser('~/bin')}:{env['PATH']}"
+        print(env)
+        p = subprocess.Popen(shell_text, shell=True, stdout=subprocess.PIPE, executable="/bin/zsh", start_new_session=True, env=env)  # 执行shell语句并定义输出格式
         while p.poll() is None:  # 判断进程是否结束（Popen.poll()用于检查子进程（命令）是否已经执行结束，没结束返回None，结束后返回状态码）
             if p.wait() != 0:  # 判断是否执行成功（Popen.wait()等待子进程结束，并返回状态码；如果设置并且在timeout指定的秒数之后进程还没有结束，将会抛出一个TimeoutExpired异常。）
                 print("命令执行失败，请检查设备连接状态")
-                return False,""
+                return False, ""
             else:
                 shell_result = p.stdout.readlines()  # 获取原始执行结果
                 result = []
