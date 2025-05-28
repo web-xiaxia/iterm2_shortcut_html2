7385c4c0711d7986fba00b9f85dbf8196d398c14
熊闯 <xiongchuang@vip.qq.com>
2025-05-28 18:42:29 +0800

    调整

--- iterm2_shortcut_html2/api/py_api.py
+++ iterm2_shortcut_html2/api/py_api.py
@@ -105,17 +105,21 @@ class PyApi:
 {shell_pre}
 {shell_text}
 """, shell=True, stdout=subprocess.PIPE, executable="/bin/zsh", start_new_session=True, env=env)  # 执行shell语句并定义输出格式
-        while p.poll() is None:  # 判断进程是否结束（Popen.poll()用于检查子进程（命令）是否已经执行结束，没结束返回None，结束后返回状态码）
-            if p.wait() != 0:  # 判断是否执行成功（Popen.wait()等待子进程结束，并返回状态码；如果设置并且在timeout指定的秒数之后进程还没有结束，将会抛出一个TimeoutExpired异常。）
-                print("命令执行失败，请检查设备连接状态")
-                return False, ""
-            else:
-                shell_result = p.stdout.readlines()  # 获取原始执行结果
-                result = []
-                for i in range(len(shell_result)):  # 由于原始结果需要转换编码，所以循环转为utf8编码并且去除\n换行
-                    res = shell_result[i].decode('utf-8').strip('\r\n')
-                    result.append(res)
-                return True, "\n".join(result)
+        try:
+            while p.poll() is None:  # 判断进程是否结束（Popen.poll()用于检查子进程（命令）是否已经执行结束，没结束返回None，结束后返回状态码）
+                if p.wait(timeout=20) != 0:  # 判断是否执行成功（Popen.wait()等待子进程结束，并返回状态码；如果设置并且在timeout指定的秒数之后进程还没有结束，将会抛出一个TimeoutExpired异常。）
+                    print("命令执行失败，请检查设备连接状态")
+                    return False, ""
+                else:
+                    shell_result = p.stdout.readlines()  # 获取原始执行结果
+                    result = []
+                    for i in range(len(shell_result)):  # 由于原始结果需要转换编码，所以循环转为utf8编码并且去除\n换行
+                        res = shell_result[i].decode('utf-8').strip('\r\n')
+                        result.append(res)
+                    return True, "\n".join(result)
+        except Exception as e:
+            print(f"exec_shell 运行失败, {e}\n{traceback.format_exc()}")
+            # 如果发生异常，返回False和空字符串
         return False, ""
 
     async def alert(self, title='', subtitle=''):
